cca8a427a7b4559b1f33170b6ee1d5591d72c109
Prithvi Rajput <prithvirajput@Prithvis-MacBook-Pro.local>
2023-10-02 02:33:35 +0530

    Added customers widget

---
+++ lib/data/customer_details.dart
@@ -0,0 +1,44 @@
+final List<Map<String, String>> customers = [
+  {
+    'name': "Prithvi Rajput",
+    'premium': '250000',
+    'p_no': '123',
+    'total_price': "2500000",
+  },
+  {
+    'name': "Prithvi Rajput",
+    'premium': '250000',
+    'p_no': '123',
+    'total_price': "2500000",
+  },
+  {
+    'name': "Prithvi Rajput",
+    'premium': '250000',
+    'p_no': '123',
+    'total_price': "2500000",
+  },
+  {
+    'name': "Prithvi Rajput",
+    'premium': '250000',
+    'p_no': '123',
+    'total_price': "2500000",
+  },
+  {
+    'name': "Prithvi Rajput",
+    'premium': '250000',
+    'p_no': '123',
+    'total_price': "2500000",
+  },
+  {
+    'name': "Prithvi Rajput",
+    'premium': '250000',
+    'p_no': '123',
+    'total_price': "2500000",
+  },
+  {
+    'name': "Prithvi Rajput",
+    'premium': '250000',
+    'p_no': '123',
+    'total_price': "2500000",
+  },
+];
--- lib/widgets/customer.dart
+++ lib/widgets/customer.dart
@@ -1,10 +1,77 @@
 import 'package:flutter/material.dart';
+import 'package:khaata/data/customer_details.dart';
 
 class Customers extends StatelessWidget {
   const Customers({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final cList = customers;
+    return ListView.builder(
+        itemCount: cList.length,
+        itemBuilder: (context, index) {
+          return Card(
+            margin: const EdgeInsets.fromLTRB(20, 5, 20, 5),
+            child: Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Row(
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Row(
+                        children: [
+                          const Text(
+                            "Name: ",
+                          ),
+                          Text(
+                            "${cList[index]['name']}",
+                            style: const TextStyle(
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ],
+                      ),
+                      Row(
+                        children: [
+                          const Text("Amount Due: "),
+                          Text(
+                            "${cList[index]['premium']}",
+                            style: const TextStyle(
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ],
+                      ),
+                      Row(
+                        children: [
+                          const Text("Plot Number: "),
+                          Text(
+                            "${cList[index]['p_no']}",
+                            style: const TextStyle(
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                  const Spacer(),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        "RS ${cList[index]['total_price']}",
+                        style: const TextStyle(
+                          fontWeight: FontWeight.bold,
+                        ),
+                      )
+                    ],
+                  )
+                ],
+              ),
+            ),
+          );
+        });
   }
 }
